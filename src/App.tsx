cfb5e109f19bac131c2045abfc5c08691119f2c8
Elizaveta Shestakova <e.shestakova@megaplan.ru>
2025-10-19 14:45:39 +0300

    game over modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import "./App.css"
+import { useEffect } from "react"
+import { useGameLogic } from "./hooks/useGameLogic"
 import Header from "./components/Header"
 import Board from "./components/Board"
-import { useGameLogic } from "./hooks/useGameLogic"
-import { useEffect } from "react"
+import GameOverModal from "./components/GameOverModal"
+import "./App.css"
 
 function App() {
     const { board, score, resetGame, slideLeft, slideRight, slideUp, slideDown, bestScore, gameIsOver } = useGameLogic()
@@ -32,7 +33,8 @@ function App() {
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-blue-100">
             <Header resetGameFunction={resetGame} />
-            <Board board={board} score={score} bestScore={bestScore} gameIsOver={gameIsOver} />
+            <Board board={board} score={score} bestScore={bestScore} />
+            <GameOverModal show={gameIsOver} score={score} bestScore={bestScore} onRestart={resetGame} />
         </div>
     )
 }
