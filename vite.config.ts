59a86a37aa8537f1349016952c9e209f437ce1ba
Elizaveta Shestakova <e.shestakova@megaplan.ru>
2025-10-19 22:35:37 +0300

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "/2048-react-ts/",
     server: {
         port: 5173
     }
